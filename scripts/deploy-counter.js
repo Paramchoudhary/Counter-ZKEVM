43b7ff7b4b164193e0bffe1eae056e9da3aa43f6
Param Choudhary <choudharyparam99@gmail.com>
2023-10-21 12:59:07 +0530

    first commit

---
+++ scripts/deploy-counter.js
@@ -0,0 +1,15 @@
+import helpers from "@nomicfoundation/hardhat-toolbox/network-helpers";
+const hre = require("hardhat");
+
+async function main() {
+    const deployedContract = await hre.ethers.deployContract("Counter");
+    await deployedContract.waitForDeployment();
+    console.log(
+        `Counter contract deployed to https://explorer.public.zkevm-test.net/address/${deployedContract.target}`
+    );
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
--- src/App.js
+++ src/App.js
@@ -1,26 +1,90 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Container from "@mui/material/Container";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import "./App.css";
+import { ethers } from "ethers";
+import Counter from "../src/Counter.json";
+const counterAddress = "0xcaf5a95759b06a2f8876539c0cef7708fc2b8283";
+
+
+console.log(counterAddress, "Counter ABI: ", Counter.abi);
 
 function App() {
   const [count, setCount] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const incrementCounter = () => {
-    // we should read currentCount from the blockchain
-    const currentCount = count;
-    setCount(currentCount + 1);
-  };
+  useEffect(() => {
+    // declare the data fetching function
+    const fetchCount = async () => {
+      const data = await readCounterValue();
+      return data;
+    };
+
+    fetchCount().catch(console.error);
+  }, []);
+
+  async function requestAccount() {
+  await window.ethereum.request({ method: "eth_requestAccounts" });
+}
+
+
+  async function readCounterValue() {
+    if (typeof window.ethereum !== "undefined") {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      console.log("provider", provider);
+      const contract = new ethers.Contract(
+        counterAddress,
+        Counter.abi,
+        provider
+      );
+      console.log("contract", contract);
+      try {
+        const data = await contract.retrieve();
+        console.log(data);
+        console.log("data: ", parseInt(data.toString()));
+        setCount(parseInt(data.toString()));
+      } catch (err) {
+        console.log("Error: ", err);
+        alert(
+          "Switch your MetaMask network to Polygon zkEVM testnet and refresh this page!"
+        );
+      }
+    }
+  }
+
+async function updateCounter() {
+  if (typeof window.ethereum !== "undefined") {
+    await requestAccount();
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    console.log({ provider });
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(counterAddress, Counter.abi, signer);
+    const transaction = await contract.increment();
+    setIsLoading(true);
+    await transaction.wait();
+    setIsLoading(false);
+    readCounterValue();
+  }
+}
+
+ const incrementCounter = async () => {
+  await updateCounter();
+};
 
   return (
     <Container maxWidth="sm">
       <Card sx={{ minWidth: 275, marginTop: 20 }}>
         <CardContent>
-          <p>Count: {count}</p>
-          <Button onClick={incrementCounter} variant="outlined">
-            +1
+          <p>ZkEvm Count: {count}</p>
+          <p>by @Param_eth </p>
+          <Button
+            onClick={incrementCounter}
+            variant="outlined"
+            disabled={isLoading}
+          >
+            {isLoading ? "loading..." : "+1"}
           </Button>
         </CardContent>
       </Card>
